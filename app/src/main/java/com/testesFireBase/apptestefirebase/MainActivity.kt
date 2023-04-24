7cd472cd435b0011c15a3099443a1296e8b46102
eleandro_alberche <eleandro.alberche@gmail.com>
2023-04-24 20:19:07 -0300

    Initial commit

---
+++ app/src/main/java/com/testesFireBase/apptestefirebase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.testesFireBase.apptestefirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
